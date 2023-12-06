d72f56e909158778a6e764471b3304d3250b941a
cijb-7724 <johokamia0@gmail.com>
2023-12-07 02:26:26 +0900

    2.6

--- study/ch02/code_2.py
+++ study/ch02/code_2.py
@@ -172,8 +172,14 @@ num_pipeline = Pipeline([
     ('std_scaler', StandardScaler())
 ])
 
-
-
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ['ocean_proximity']
+full_pipeline = ColumnTransformer([
+    ('num', num_pipeline, num_attribs),
+    ('cat', OneHotEncoder(), cat_attribs),
+])
+housing_prepared = full_pipeline.fit_transform(housing)
 
 
 
