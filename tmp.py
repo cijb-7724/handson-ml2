1d4b834ef9208e136fff05255693804c82fcebaf
cijb-7724 <johokamia0@gmail.com>
2024-02-05 16:03:14 +0900

    delete

--- tmp.py
+++ tmp.py
@@ -2,3 +2,11 @@ import tensorflow as tf
 # from tensorflow import keras
 print(tf.__version__)
 # print(keras.__version__)
+# from tensorflow.python import pywrap_tensorflow as tf
+# print(tf.__version__)
+
+# pipインストール
+# C:\Users\cotan\AppData\Local\Programs\Python\Python311\Scripts\pip.exe install tensorflow
+# python実行
+# C:\Users\cotan\AppData\Local\Programs\Python\Python311\python.exe tmp.py
+
